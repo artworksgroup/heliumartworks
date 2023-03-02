2eddd98039a44cbe6f062ec862447d37361fdbfc
Charles DZADU <dkccharlie@gmail.com>
2023-03-02 14:36:05 +0000

    Fix: Configuration

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-4">
       <!-- Create a fancy card to welcome user -->
-      <div class="text-xl">{{$t('hello')}} To man Helium artworks {{ locale }} </div>
+      <div class="text-xl">{{$t('hello')}} To man this happen Helium artworks {{ locale }} </div>
     </div>
   </template>
   
